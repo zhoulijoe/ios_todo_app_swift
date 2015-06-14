3f38ea1b7f30f179c6848a962bdf469d23cb170b
Zhou Li <zhoulijoe@gmail.com>
2015-06-14 12:32:24 -0700

    Start with single view app template

---
+++ README.md
@@ -0,0 +1,1 @@
+# ios_todo_app_swift
---
+++ ToDoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoSwift
+//
+//  Created by Zhou on 6/14/15.
+//  Copyright (c) 2015 Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
