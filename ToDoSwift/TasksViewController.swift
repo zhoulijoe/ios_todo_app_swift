1d7c182eb4f82007b61f4b03571a51f6bf5b4a60
Zhou Li <zhoulijoe@gmail.com>
2015-06-15 09:41:56 -0700

    Add color to table rows

--- ToDoSwift/TasksViewController.swift
+++ ToDoSwift/TasksViewController.swift
@@ -10,8 +10,13 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
         tableView.delegate = self
         tableView.dataSource = self
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "taskCell")
+
+        tableView.separatorStyle = .None
+        tableView.rowHeight = 50.0
     }
-    
+
+    // MARK: - Table view data source
+
     func numberOfSectionsInTableView(_tableView: UITableView) -> Int {
         return 1
     }
@@ -27,6 +32,21 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
         cell.textLabel?.text = task.description
 
         return cell
+    }
+
+    // MARK: - Table view delegate
+
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell,
+        forRowAtIndexPath indexPath: NSIndexPath) {
+            cell.backgroundColor = colorForIndex(indexPath.row)
+    }
+
+    // MARK: - Helper methods
+
+    func colorForIndex(index: Int) -> UIColor {
+        let itemCount = TaskStore.sharedInstance.tasks.count - 1
+        let val = (CGFloat(index) / CGFloat(itemCount)) * 0.6
+        return UIColor(red: 1.0, green: val, blue: 0.0, alpha: 1.0)
     }
 }
 
